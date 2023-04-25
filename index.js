980ae3e033ad2415af9522ee4d6f7087ab074106
AelinaXY <Lauren@2infinity.net>
2023-04-25 16:13:54 +0100

    Update CRUD to mongo functionality

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 
+const catRoutes = require("./routes/cats.js");
+const catDB = require("./db.js");
+
 //Top
 const app = express();
 
-const cats = ['JSON', 'XML', 'Cat cat = new Cat()'];
 
 const logger = (req,res, next) => {
 console.log("Host: ", req.hostname);
@@ -17,29 +19,8 @@ app.use(logger);
 
 app.use(express.json());
 
-app.get('/getAll', (req,res) => {res.json(cats)});
 
-app.post('/create',(req,res) =>{cats.push(req.body.name);
-res.status(201).json(cats[cats.length-1]);});
-
-app.delete('/remove/:id', (req,res) =>
-{
-    const {id} = req.params;
-    const deadCat = cats.splice(id,1);
-    res.status(200).json(deadCat);
-})
-
-app.patch('/update/:id', (req,res,next) =>
-{
-    const {id} = req.params;
-    const {name} = req.query;
-    if(id >= cats.length){
-        return next({msg:`ERROR: out of bounds: ${id}`,status:404});
-    }
-    cats[id] = name; 
-    res.status(200).json(cats[id]);
-})
-
+app.use("/cats",catRoutes);
 
 app.use((err,req,res,next) =>
 {
