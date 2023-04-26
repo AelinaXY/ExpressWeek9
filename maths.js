bef14fa46289305e8dadc68d33f163cff480d968
AelinaXY <Lauren@2infinity.net>
2023-04-26 10:05:59 +0100

    Add prime func and tests

---
+++ maths.js
@@ -0,0 +1,14 @@
+const prime = (a) =>
+{
+    if(a==1){return false;}
+    for (let i = 2; i<a/2;i++)
+    {
+        if(a%i==0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+module.exports = prime;
